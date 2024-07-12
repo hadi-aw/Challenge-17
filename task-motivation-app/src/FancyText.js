7f9583ba729309850d4ae6be1e7baf86b056a8c8
Hadi Awad <hawad.abdrabbou@gmail.com>
2024-07-12 19:21:22 -0400

    index.html

---
+++ task-motivation-app/src/FancyText.js
@@ -0,0 +1,14 @@
+// U51313007
+// src/FancyText.js
+import React from 'react';
+import './FancyText.css';
+
+const FancyText = ({ title, text }) => {
+    return (
+        <div className={title ? 'title' : 'regular'}>
+            {text}
+        </div>
+    );
+}
+
+export default FancyText;
---
+++ task-motivation-app/src/tasks.js
@@ -0,0 +1,9 @@
+// U51313007
+// src/tasks.js
+const tasks = [
+    { id: 1, name: "complete the React challenge", isCompleted: false },
+    { id: 2, name: "Review project documentation", isCompleted: false },
+    { id: 3, name: "Refactor the codebase", isCompleted: false },
+];
+
+export default tasks;
